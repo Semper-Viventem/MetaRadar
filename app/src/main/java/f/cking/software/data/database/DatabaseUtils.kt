c4b364eb19ff3732d97db46af0308bd6c3ecfd20
konstantin <konstantin.kulikov@bolt.eu>
2023-02-23 10:51:33 +0200

    Update javadoc

--- app/src/main/java/f/cking/software/data/database/DatabaseUtils.kt
+++ app/src/main/java/f/cking/software/data/database/DatabaseUtils.kt
@@ -5,6 +5,8 @@ object DatabaseUtils {
     /**
      * To prevent excessive memory allocations, the maximum value of a host parameter number is SQLITE_MAX_VARIABLE_NUMBER,
      * which defaults to 999 for SQLite versions prior to 3.32.0 (2020-05-22) or 32766 for SQLite versions after 3.32.0.
+     *
+     * https://www.sqlite.org/limits.html
      */
     const val MAX_SQL_VARIABLES = 32766
 }
