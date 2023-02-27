881cf979531e0dcbdf258b0f5828d8970b865a13
Semper-Viventem <kostyaxxx8@gmail.com>
2023-02-27 05:14:59 +0200

    Update ci scripts

--- .github/actions/publish-release-apk/entrypoint.sh
+++ .github/actions/publish-release-apk/entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 curl -sSL https://get.docker.com/ | sudo sh
-docker pull mingc/android-build-box:latest
 hub clone https://github.com/Semper-Viventem/MetaRadar.git
 hub checkout ${${REPO_BRANCH}:-master}
 chmod +x gradlew
-docker run --rm -v `pwd`:/project mingc/android-build-box bash -c 'cd /project; ./gradlew assembleDebug'
+docker run --rm -v `pwd`:/project mingc/android-build-box bash -c "cd /project; ./gradlew ${assembleDebug}"
 VERSION_NAME=`grep -oP 'versionName = "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle.kts`
 hub release create -a ./${APP_FOLDER}/build/outputs/apk/${BUILD_TYPE}/*-${BUILD_TYPE}.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
