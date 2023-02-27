5ea0c35cbb73c57abdaef557091ae3cd03296117
Semper-Viventem <kostyaxxx8@gmail.com>
2023-02-27 04:25:49 +0200

    Update ci scripts

--- .github/actions/publish-release-apk/entrypoint.sh
+++ .github/actions/publish-release-apk/entrypoint.sh
@@ -5,4 +5,4 @@ hub checkout ${${REPO_BRANCH}:-master}
 chmod +x gradlew
 ./gradlew ${GRADLEW_CONFIG}
 VERSION_NAME=`grep -oP 'versionName = "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle.kts`
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/BUILD_TYPE/*-BUILD_TYPE.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
+hub release create -a ./${APP_FOLDER}/build/outputs/apk/${BUILD_TYPE}/*-${BUILD_TYPE}.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
