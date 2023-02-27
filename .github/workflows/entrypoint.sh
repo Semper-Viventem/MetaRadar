a92b6da2dfa308670d51fc48287d87dfe545f031
Semper-Viventem <kostyaxxx8@gmail.com>
2023-02-27 03:27:13 +0200

    Update ci scripts

--- .github/workflows/entrypoint.sh
+++ .github/workflows/entrypoint.sh
@@ -3,4 +3,4 @@
 hub clone https://github.com/Semper-Viventem/MetaRadar.git
 hub checkout ${${REPO_BRANCH}:-master}
 VERSION_NAME=`grep -oP 'versionName = "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle.kts`
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/*-debug.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
+hub release create -a ./${APP_FOLDER}/build/outputs/apk/BUILD_TYPE/*-BUILD_TYPE.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
