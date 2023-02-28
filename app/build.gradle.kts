4d7e0fd4227213c147f7edf1430650f8fb09eb9d
konstantin <konstantin.kulikov@bolt.eu>
2023-02-28 12:33:40 +0200

    Update min lost time filter template

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 29
         targetSdk = 33
         versionCode = (System.currentTimeMillis() / 1000).toInt()
-        versionName = "0.3-alpha"
+        versionName = "0.4-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
