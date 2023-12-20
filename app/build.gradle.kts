5e9f6c74912dfb7be8ca8e8a05fe9a9219a2493e
Semper-Viventem <kostyaxxx8@gmail.com>
2023-12-20 18:48:24 +0200

    Update the app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = (System.currentTimeMillis() / 1000).toInt()
-        versionName = "0.20.1-beta"
+        versionName = "0.20.2-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
