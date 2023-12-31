270b88529832e3a8d3a279cd06b71c9344c057bd
Semper-Viventem <kostyaxxx8@gmail.com>
2023-12-31 15:40:26 +0200

    Fix tagging and add some metadata

--- .github/actions/publish-release-apk/entrypoint.sh
+++ .github/actions/publish-release-apk/entrypoint.sh
@@ -4,5 +4,5 @@ ls -a
 git config --global --add safe.directory /github/workspace
 hub checkout ${${REPO_BRANCH}:-master}
 VERSION_NAME=`grep -oP 'versionName = "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle.kts`
-TAG = "v${VERSION_NAME}${TAG_SUFFIX}"
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/${BUILD_FLAVOR}/${BUILD_TYPE}/*-${BUILD_TYPE}.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" ${TAG}
+TAG_NAME = "v${VERSION_NAME}${TAG_SUFFIX}"
+hub release create -a ./${APP_FOLDER}/build/outputs/apk/${BUILD_FLAVOR}/${BUILD_TYPE}/*-${BUILD_TYPE}.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" ${${TAG_NAME}}
