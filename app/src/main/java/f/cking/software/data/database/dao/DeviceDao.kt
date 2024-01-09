f5a6f13568076a6d7ed9b8ff2cf08da8ccb80363
Semper-Viventem <kostyaxxx8@gmail.com>
2024-01-10 00:59:46 +0200

    Add devices pagination

--- app/src/main/java/f/cking/software/data/database/dao/DeviceDao.kt
+++ app/src/main/java/f/cking/software/data/database/dao/DeviceDao.kt
@@ -1,6 +1,9 @@
 package f.cking.software.data.database.dao
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import f.cking.software.data.database.entity.DeviceEntity
 
 @Dao
@@ -11,6 +14,9 @@ interface DeviceDao {
 
     @Query("SELECT * FROM device WHERE address LIKE :address")
     fun findByAddress(address: String): DeviceEntity?
+
+    @Query("SELECT * FROM device ORDER BY last_detect_time_ms DESC LIMIT :limit OFFSET :offset")
+    fun getPaginated(offset: Int, limit: Int): List<DeviceEntity>
 
     @Query("SELECT * FROM device WHERE address IN (:addresses)")
     fun findAllByAddresses(addresses: List<String>): List<DeviceEntity>
