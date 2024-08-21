9fd6400a648fe28e874cfc750756063228d51085
Konstantin <kostyaxxx8@gmail.com>
2024-08-20 18:37:28 -0700

    Fix Github Action

--- .github/actions/publish-release-apk/entrypoint.sh
+++ .github/actions/publish-release-apk/entrypoint.sh
@@ -4,4 +4,4 @@ ls -a
 git config --global --add safe.directory /github/workspace
 hub checkout ${REPO_BRANCH:-master}
 VERSION_NAME=`grep -oP 'versionName = "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle.kts`
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/${BUILD_FLAVOR}/${BUILD_TYPE}/*-${BUILD_TYPE}.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" v${VERSION_NAME}${TAG_SUFFIX}
+hub release create -t ${REPO_BRANCH:-master} -a ./${APP_FOLDER}/build/outputs/apk/${BUILD_FLAVOR}/${BUILD_TYPE}/*-${BUILD_TYPE}.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" v${VERSION_NAME}${TAG_SUFFIX}
